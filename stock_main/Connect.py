12064d042023306a0a5c6a772e5efcc7a1d24858
leenuu <61344683+leenuu@users.noreply.github.com>
2021-08-30 23:10:04 +0900

    Add files via upload

--- stock_main/Connect.py
+++ stock_main/Connect.py
@@ -21,7 +21,7 @@ class cybos_connect:
             app.start(f'C:\DAISHIN\STARTER\\ncStarter.exe /prj:cp /id:{id} /pwd:{pwd} /pwdcert:{pwdcert} /autostart')
         elif status == False:
             app.start(f'C:\DAISHIN\STARTER\\ncStarter.exe /prj:cp /id:{id} /pwd:{pwd} /pwdcert: /autostart')            
-        time.sleep(30)
+        self.timer()
     
     def connect_test(self):
         objCpCybos = win32com.client.Dispatch("CpUtil.CpCybos")
@@ -31,3 +31,8 @@ class cybos_connect:
             exit()
         else:
             print("PLUS Connect Complete")
+    
+    def timer(self):
+        for i in range(60):
+            print(f"\r{60 - i} ")
+            time.sleep(1)
--- stock_main/stock_main.py
+++ stock_main/stock_main.py
@@ -3,7 +3,7 @@ import os
 import sys
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 from security.encryption_process import encryption_pro
-
+import win32com.client
 
 class stock:
     def __init__(self, status):
@@ -11,8 +11,12 @@ class stock:
         self.pwd = ''
         self.pwdcert = ''
         self.user_inform = dict()
+        self.fild = []
+        self.fild_dict = {}
+        self.stock_data = dict()
+        self.stock_code = list()
         self.status = status
-    
+
     def login(self):
         login_system = encryption_pro()
         self.user_inform = login_system.get_key_file()
@@ -29,9 +33,37 @@ class stock:
 
         # print("connect test complete")
     
+    def add_code(self, codes):
+        if type(codes) == list:
+            for code in codes:
+                if code not in self.stock_code:
+                    self.stock_code.append(code)
+        elif type(codes) == str: 
+            if codes not in self.stock_code:
+                self.stock_code.append(codes)    
+
+    def all_stocks_data(self, code):
+        stock_data = win32com.client.Dispatch("CpSysDib.MarketEye")
+        stock_data.SetInputValue(0, self.fild)    
+        stock_data.SetInputValue(1, [code])
+        stock_data.BlockRequest()
+ 
+        
+        
+    def check_all_stocks(self):
+        objCpCodeMgr = win32com.client.Dispatch("CpUtil.CpCodeMgr")
+        codeList = objCpCodeMgr.GetStockListByMarket(1) #거래소, 코스피
+        strr = ''
+        for i, code in enumerate(codeList):
+            secondCode = objCpCodeMgr.GetStockSectionKind(code)
+            name = objCpCodeMgr.CodeToName(code)
+            stdPrice = objCpCodeMgr.GetStockStdPrice(code)
+            strr += str(name) + ' ' + str(secondCode) + ' ' + str(stdPrice) + '\n'
+        print(i, code, secondCode, stdPrice, name)
 
 test = stock(False)
-test.login()
-# test.connect_test()
+# test.login()
+test.all_stocks_data('A302440')
+print(test.stock_data)
 
 
