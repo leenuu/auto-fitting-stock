a03f9ffe6e756de985a8ad3aac8358717b3db2ad
leenuu <61344683+leenuu@users.noreply.github.com>
2021-09-08 13:32:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from os import path
-from dynamic_import import dynamic_import
-import os
+from stock_main.stock_main import stock
 
 if __name__ == "__main__":
-    path = str(os.getcwd()) + "\\stock_main"
-    dynamic = dynamic_import.dynamic_import(path)
-    dynamic.run()
+    stocks = stock()
+    stocks.run()
+    pass
